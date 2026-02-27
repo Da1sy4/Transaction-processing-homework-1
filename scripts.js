0b419b862ad076d11ed1ee31eb393ca3637e2f41
Da1sy4 <quedaisy156@gmail.com>
2026-02-27 03:30:17 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
+/*
+Program name- Cura Cardiology Clinic patient form.html 
+Name- Daisy Que
+date created-2/24/2026
+version 1.0-2/24/26, 2.0-2/25/26,
+Description- This document will be the javascript portion of hw1 
+NOTE: I used the TP resources from MISSO but I used my own variables 
+*/
+/* for date*/
+const d= new Date ();
+let text=d.toLocaleDateString();
+document.getElementById("today").innerHTML=text;
 
+/*for slider*/
+let slider=document.getElementById("range")
+let output=document.getElementById("range-scale")
+output.innerHTML=slider.value;
+slider.oninput=function() {output.innerHTML=this.value;};
